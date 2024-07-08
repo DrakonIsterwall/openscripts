a5374cafd2af11974ce51a3b81aac30c6fe3f18e
Grey Guy <117237001+DrakonIsterwall@users.noreply.github.com>
2024-07-08 22:23:57 +0200

    Create RDP.sh

---
+++ RDP.sh
@@ -0,0 +1,6 @@
+#A script to easier connect over RDP to a Windows System
+#xfreerdp /u:username /p:password /v:machine_ip /dynamic-resolution
+#thats the basic idea
+#xfreerdp /u:administrator /p:letmein123! /v:x.x.x.x /dynamic-resolution
+
+Soon...
