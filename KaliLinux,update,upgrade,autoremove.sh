89b11e7b17ece336953f03239b53dd05f70d7585
Grey Guy <117237001+DrakonIsterwall@users.noreply.github.com>
2024-07-08 22:34:49 +0200

    Update KaliLinux,update,upgrade,autoremove.sh

--- KaliLinux,update,upgrade,autoremove.sh
+++ KaliLinux,update,upgrade,autoremove.sh
@@ -3,3 +3,6 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 #If you want to full upgrade too through this bash script then uncomment the 5th line
 #sudo apt full-upgrade -y
+
+#Just for Information: If you are using last-snapshot, you will not receive updates until we release the next version of Kali for that year. For this reason,
+#it is a good idea to follow the Kali Twitter, or check the Kali website every few months. Kali has a release four times a year, and follows a loose quarterly schedule.
