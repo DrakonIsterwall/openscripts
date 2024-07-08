a82033458e3d0861d81e5f71de920544a4e5c54d
Grey Guy <117237001+DrakonIsterwall@users.noreply.github.com>
2024-07-08 22:32:45 +0200

    Update KaliLinux,update,upgrade,autoremove.sh

--- KaliLinux,update,upgrade,autoremove.sh
+++ KaliLinux,update,upgrade,autoremove.sh
@@ -1,2 +1,5 @@
 #A bash script for updating,uprading and autoremoving files
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+
+#If you want to full upgrade too through this bash script then uncomment the 5th line
+#sudo apt full-upgrade -y
