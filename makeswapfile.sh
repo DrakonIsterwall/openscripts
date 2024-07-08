f6d4f24507a253531386751c492a0e6dbe051c34
Grey Guy <117237001+DrakonIsterwall@users.noreply.github.com>
2024-07-08 22:37:43 +0200

    Update makeswapfile.sh

--- makeswapfile.sh
+++ makeswapfile.sh
@@ -1,3 +1,5 @@
+#When you have no idea what a swapfile is then read the following link: https://linuxize.com/post/create-a-linux-swap-file/
+
 #change 10000 for making swapfile larger or smaller
 dd if=/dev/zero of=/swapfile bs=1MB count=10000
 chmod 600 /swapfile
@@ -6,5 +8,5 @@ swapon /swapfile
 swapon --show
 #make swapfile persistent
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab 
-#make swapiness persisttent value 0-200 ( how likely the system gonna use the swapfile)
+#make swapiness persistent value 0-200 ( how likely the system gonna use the swapfile)
 echo "vm.swappiness = 100" >> /etc/sysctl.conf
