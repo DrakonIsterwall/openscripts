d3cfc34f3acb2198789f447db211017a2b0781c1
Grey Guy <117237001+DrakonIsterwall@users.noreply.github.com>
2024-07-28 19:04:05 +0200

    Update wifirestart.sh

--- wifirestart.sh
+++ wifirestart.sh
@@ -1,4 +1,4 @@
 #make your .sh file executable and you have to use chmod u+x on this .sh file
-!/bin/bash
+#!/bin/bash
 #A bash script to restart the NetworkManager since sometimes after hibernate my laptop doesnt show wifi in my Kali Linux.
 service NetworkManager restart
